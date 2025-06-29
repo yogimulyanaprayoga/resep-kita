c91ddb33c89de808d563e555689c9a7c680f25bd
unknown <yogimulyana498@gmail.com>
2025-06-29 15:31:43 +0700

    configuration tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {},
+    plugins: [require("daisyui")],
+};
